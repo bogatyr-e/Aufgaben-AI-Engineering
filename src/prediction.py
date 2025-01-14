a057bbe8f7c0cd981acd8ed3769d39118af9f584
bogatyr-e <bogatyre@googlemail.com>
2025-01-14 23:45:36 +0100

    prediction-data.csv hinzugefügt

--- src/prediction.py
+++ src/prediction.py
@@ -6,7 +6,8 @@ file_to_open = open("data/models/regression_lr.pickle", 'rb')
 trained_model = pickle.load(file_to_open)
 file_to_open.close()
 
-# load data that we want predictions for #es muss noch ein 'prediction-data.csv' file geben hierfür; vom train.py generiert
+# load data that we want predictions for
+# #es muss noch ein 'prediction-data.csv' file geben hierfür; vom train.py generiert, aber wo?
 prediction_data = pd.read_csv('data/prediction-data.csv', sep=";")
 
 print(trained_model.predict(prediction_data))
