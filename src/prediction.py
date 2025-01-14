69ec81160c2b488ef32ecee2b7ea497a1d3d5ac4
bogatyr-e <bogatyre@googlemail.com>
2025-01-15 00:20:45 +0100

    .py's erneut ausgeführt

--- src/prediction.py
+++ src/prediction.py
@@ -2,12 +2,13 @@ import pandas as pd
 import pickle
 
 # load trained model
-file_to_open = open("data/models/regression_lr.pickle", 'rb')
+file_to_open = open("data/models/lin-regression.pickle", 'rb')
 trained_model = pickle.load(file_to_open)
 file_to_open.close()
 
 # load data that we want predictions for
 # #es muss noch ein 'prediction-data.csv' file geben hierfür; vom train.py generiert, aber wo?
+#.csv von Laura copiert, seufz.
 prediction_data = pd.read_csv('data/prediction-data.csv', sep=";")
 
 print(trained_model.predict(prediction_data))
--- src/train.py
+++ src/train.py
@@ -27,5 +27,5 @@ regressor = regressor.fit(x_train, y_train)
 
 y_pred = regressor.predict(x_test)
 
-file_to_write = open("data/models/regression_lr.pickle", "wb")
+file_to_write = open("data/models/lin-regression.pickle", "wb")
 pickle.dump(regressor, file_to_write)
